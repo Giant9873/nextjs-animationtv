71e9d052eacc370fcb79cbc54d6c37999a2f85e9
Giant9873 <gian.torres099@gmail.com>
2022-10-08 18:56:24 -0500

    tercera actualizacion

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,12 +3,13 @@ import styles from '../styles/Home.module.css';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-export default function Layout({ children , title, description }) {
+export default function Layout({ children , title, description, imageog }) {
   return (
     <div>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="og:image" content={"https://nextjs-animationtv.vercel.app/"+imageog} />
       </Head>
 
       <Navbar />
@@ -22,5 +23,6 @@ export default function Layout({ children , title, description }) {
 
 Layout.defaulProps = {
     title: "Animation TV",
-    description: "Lo mejor de la animación en una sola web"
+    description: "Lo mejor de la animación en una sola web",
+    imageog: "homepri.jpg" 
 };
--- pages/blog/homecard.jsx
+++ pages/blog/homecard.jsx
@@ -3,15 +3,23 @@ import Link from "next/link";
 
 const MENU_LIST = [
   {
-      id: "1",
+      id: 12,
+      titulo: "Mob Psycho 100 III",
+      tipo: "anime",
+      href: "/6341e1a528f84e5af3593027",
+      url: "/img/poster/mob p 100 s3 - poster.jpg",
+      año: "2022",
+      capitulos: "01/??"
+  },{
+      id: 11,
       titulo: "Spy x Family S2",
       tipo: "anime",
       href: "/633b6161d381cf953bbf16a0",
       url: "/img/poster/spy x family s2 - poster.jpg",
       año: "2022",
-      capitulos: "01/13"
+      capitulos: "02/13"
   },{
-      id: "2",
+      id: 10,
       titulo: "Classroom of the Elite II",
       tipo: "anime",
       href: "/63336a5b35ff664c825d952e",
@@ -19,7 +27,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "13/13"
   },{
-      id: "3",
+      id: 9,
       titulo: "Overlord IV",
       tipo: "anime",
       href: "/6334a6d404ae9bd3a83ac935",
@@ -27,7 +35,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "13/13"
   },{
-      id: "4",
+      id: 8,
       titulo: "Utawarerumono: Futari no Hakuoro",
       tipo: "anime",
       href: "/632b854f4180c5cd4f97ed8a",
@@ -35,7 +43,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "14/28"
   },{
-      id: "5",
+      id: 7,
       titulo: "Lycoris Recoil",
       tipo: "anime",
       href: "/63352062cc5e3377d6a6a416",
@@ -43,7 +51,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "13/13"
   },{
-      id: "6",
+      id: 6,
       titulo: "Engage Kiss",
       tipo: "anime",
       href: "/6335cc17cc5e3377d6a6a41f",
@@ -51,7 +59,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "13/13"
   },{
-      id: "7",
+      id: 5,
       titulo: "Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka IV: Shin Shou - Meikyuu-hen",
       tipo: "anime",
       href: "/632b854f4180c5cd4",
@@ -59,15 +67,15 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "09/11"
   },{
-      id: "8",
+      id: 4,
       titulo: "Vermeil in Gold",
       tipo: "anime",
-      href: "/632b854f4180c5cd4",
+      href: "/6341bb6628f84e5af359301f",
       url: "/img/poster/vermeil in gold - poster.jpg",
       año: "2022",
       capitulos: "12/12"
   },{
-      id: "9",
+      id: 3,
       titulo: "Summertime Render",
       tipo: "anime",
       href: "/632b854f4180c5cd4",
@@ -75,7 +83,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "22/25"
   },{
-      id: "003",
+      id: 2,
       titulo: "Ao Ashi",
       tipo: "anime",
       href: "/632b854f4180c5cd4",
@@ -83,7 +91,7 @@ const MENU_LIST = [
       año: "2022",
       capitulos: "22/24"
   },{
-      id: '002',
+      id: 1,
       titulo: "Tomodadadchi Game",
       tipo: "anime",
       href: "/632b854f4180c5cd4",
@@ -97,12 +105,12 @@ const Homecard = () => {
   return(
       <div className="row" >
       {MENU_LIST.map((menu, idx) => (
-        <Link href={menu.href} key={menu.titulo}>
+        <Link href={menu.href} key={menu.id}>
         <div className="col-md-3 col-sm-3" >
             <div className="card" >
               <Image className="card-img-top crop2" src={menu.url} width={600} height={400} alt="1"></Image>
               <div className="card-body">
-                <h5 className="card-title">{menu.titulo} - {menu.capitulo}</h5>
+                <h5 className="card-title">{menu.titulo} - {menu.capitulos}</h5>
                 <p className="card-text">{menu.tipo} - {menu.año}</p>
               </div>
             </div>
